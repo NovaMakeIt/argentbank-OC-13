4b539f152c0895b1ac03f784a532c7b963fec0e4
NOVA <147254516+NovaMakeIt@users.noreply.github.com>
2025-07-08 20:36:05 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import User from './pages/User';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/user" element={<User />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
